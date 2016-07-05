645987603b92bc239ce5eeacd3d2ef46c59a9baf
Chimo <chimo@chromic.org>
2016-07-05 11:57:19 -0400

    Add state/props tied to search box content

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,11 +7,22 @@ import SearchForm from './SearchFormComponent.js';
 import SearchResults from './SearchResultsComponent.js';
 
 class AppComponent extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searchText: ''
+        };
+    }
+    handleSearch(searchText) {
+        this.setState({
+            searchText: searchText
+        });
+    }
     render() {
         return (
         	<div>
-	            <SearchForm />
-	            <SearchResults />
+	            <SearchForm onSearch={this.handleSearch.bind(this)} />
+	            <SearchResults searchText={this.state.searchText} />
 	        </div>
         );
     }
--- src/components/SearchFormComponent.js
+++ src/components/SearchFormComponent.js
@@ -5,11 +5,18 @@ import React from 'react';
 require('styles//SearchForm.scss');
 
 class SearchFormComponent extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.props.onSearch(
+      this.refs.searchTextInput.value
+    );
+  }
   render() {
     return (
-      <form className="search-form js-search-form">
+      <form className="search-form js-search-form" onSubmit={this.handleSubmit.bind(this)}>
         <div className="search__field-button">
-          <input className="search-field" name="q" placeholder="Find Social Enterprises" type="search" />
+          <input className="search-field" name="q" placeholder="Find Social Enterprises" type="search" ref="searchTextInput" />
           <input className="search-button" type="submit" value="Search" />
         </div>
       </form>
