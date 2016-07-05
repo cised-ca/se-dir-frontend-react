1686a9245757265c49204b34b4168651262b623f
Chimo <chimo@chromic.org>
2016-07-05 12:42:17 -0400

    js/css: Fix linting issues

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,41 +7,41 @@ import SearchForm from './SearchFormComponent.js';
 import SearchResults from './SearchResultsComponent.js';
 
 class AppComponent extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            searchText: ''
-        };
-    }
-    handleSearch(searchText) {
-        /**
-         * FIXME: This shouldn't be here, just using
-         *        this space as a test
-         */
-        var intro = document.querySelector('.js-intro'),
-            search_results = document.querySelector('.js-search-results');
+  constructor() {
+    super();
+    this.state = {
+      searchText: ''
+    };
+  }
+  handleSearch(searchText) {
+    /**
+     * FIXME: This shouldn't be here, just using
+     *        this space as a test
+     */
+    var intro = document.querySelector('.js-intro'),
+      search_results = document.querySelector('.js-search-results');
 
-        intro.classList.add('slide-up');
+    intro.classList.add('slide-up');
 
-        window.setTimeout(function() {
-            search_results.classList.add('fade-in');
-        }, 1000);
-        /**
-         * End FIXME
-         */
+    window.setTimeout(function() {
+      search_results.classList.add('fade-in');
+    }, 1000);
+    /**
+     * End FIXME
+     */
 
-        this.setState({
-            searchText: searchText
-        });
-    }
-    render() {
-        return (
-        	<div>
-	            <SearchForm onSearch={this.handleSearch.bind(this)} />
-	            <SearchResults searchText={this.state.searchText} />
-	        </div>
+    this.setState({
+      searchText: searchText
+    });
+  }
+  render() {
+    return (
+        <div>
+        <SearchForm onSearch={this.handleSearch.bind(this)} />
+        <SearchResults searchText={this.state.searchText} />
+        </div>
         );
-    }
+  }
 }
 
 AppComponent.defaultProps = {
--- src/components/SearchResultsComponent.js
+++ src/components/SearchResultsComponent.js
@@ -1,74 +1,74 @@
 'use strict';
 
 var directory = {
-	listOfEnterprises: [
-		{
-			'_id' : '1',
-			'title' : 'enterprise 1',
-			'description' : 'a place where lot\'s of people go to work and get things done description',
-			'offering' : 'Bob Smith',
-			'phone' : 27777777777,
-			'email' : 'test1@test.com',
-			'primary-purpose' : 'kjljlk;kj',
-			'secondary-purpose' : 'whateve',
-			'website' : 'http://google.com'
-		},
-		{
-			'_id' : '2',
-			'title' : 'enterprise 2',
-			'description' : 'description2',
-			'offering' : 'Bob Smith',
-			'phone' : 2777777779,
-			'email' : 's@s.com',
-			'primary-purpose' : 'a primary-purpose',
-			'secondary-purpose' : 'whatevekk',
-			'website' : 'http://giggle.com'
-		},
-		{
-			'_id' : '1',
-			'title' : 'enterprise 1',
-			'description' : 'a place where lot\'s of people go to work and get things done description',
-			'offering' : 'Bob Smith',
-			'phone' : 27777777777,
-			'email' : 'test1@test.com',
-			'primary-purpose' : 'kjljlk;kj',
-			'secondary-purpose' : 'whateve',
-			'website' : 'http://google.com'
-		},
-		{
-			'_id' : '1',
-			'title' : 'enterprise 1',
-			'description' : 'a place where lot\'s of people go to work and get things done description',
-			'offering' : 'Bob Smith',
-			'phone' : 27777777777,
-			'email' : 'test1@test.com',
-			'primary-purpose' : 'kjljlk;kj',
-			'secondary-purpose' : 'whateve',
-			'website' : 'http://google.com'
-		},
-		{
-			'_id' : '1',
-			'title' : 'enterprise 1',
-			'description' : 'a place where lot\'s of people go to work and get things done description',
-			'offering' : 'Bob Smith',
-			'phone' : 27777777777,
-			'email' : 'test1@test.com',
-			'primary-purpose' : 'kjljlk;kj',
-			'secondary-purpose' : 'whateve',
-			'website' : 'http://google.com'
-		},
-		{
-			'_id' : '1',
-			'title' : 'enterprise 1',
-			'description' : 'a place where lot\'s of people go to work and get things done description',
-			'offering' : 'Bob Smith',
-			'phone' : 27777777777,
-			'email' : 'test1@test.com',
-			'primary-purpose' : 'kjljlk;kj',
-			'secondary-purpose' : 'whateve',
-			'website' : 'http://google.com'
-		}
-	]
+  listOfEnterprises: [
+    {
+      '_id' : '1',
+      'title' : 'enterprise 1',
+      'description' : 'a place where lot\'s of people go to work and get things done description',
+      'offering' : 'Bob Smith',
+      'phone' : 27777777777,
+      'email' : 'test1@test.com',
+      'primary-purpose' : 'kjljlk;kj',
+      'secondary-purpose' : 'whateve',
+      'website' : 'http://google.com'
+    },
+    {
+      '_id' : '2',
+      'title' : 'enterprise 2',
+      'description' : 'description2',
+      'offering' : 'Bob Smith',
+      'phone' : 2777777779,
+      'email' : 's@s.com',
+      'primary-purpose' : 'a primary-purpose',
+      'secondary-purpose' : 'whatevekk',
+      'website' : 'http://giggle.com'
+    },
+    {
+      '_id' : '1',
+      'title' : 'enterprise 1',
+      'description' : 'a place where lot\'s of people go to work and get things done description',
+      'offering' : 'Bob Smith',
+      'phone' : 27777777777,
+      'email' : 'test1@test.com',
+      'primary-purpose' : 'kjljlk;kj',
+      'secondary-purpose' : 'whateve',
+      'website' : 'http://google.com'
+    },
+    {
+      '_id' : '1',
+      'title' : 'enterprise 1',
+      'description' : 'a place where lot\'s of people go to work and get things done description',
+      'offering' : 'Bob Smith',
+      'phone' : 27777777777,
+      'email' : 'test1@test.com',
+      'primary-purpose' : 'kjljlk;kj',
+      'secondary-purpose' : 'whateve',
+      'website' : 'http://google.com'
+    },
+    {
+      '_id' : '1',
+      'title' : 'enterprise 1',
+      'description' : 'a place where lot\'s of people go to work and get things done description',
+      'offering' : 'Bob Smith',
+      'phone' : 27777777777,
+      'email' : 'test1@test.com',
+      'primary-purpose' : 'kjljlk;kj',
+      'secondary-purpose' : 'whateve',
+      'website' : 'http://google.com'
+    },
+    {
+      '_id' : '1',
+      'title' : 'enterprise 1',
+      'description' : 'a place where lot\'s of people go to work and get things done description',
+      'offering' : 'Bob Smith',
+      'phone' : 27777777777,
+      'email' : 'test1@test.com',
+      'primary-purpose' : 'kjljlk;kj',
+      'secondary-purpose' : 'whateve',
+      'website' : 'http://google.com'
+    }
+  ]
 };
 
 import React from 'react';
@@ -80,20 +80,20 @@ class SearchResultsComponent extends React.Component {
     var list = directory.listOfEnterprises;
 
     return (
-      <ol className='search-results js-search-results'>
+        <ol className='search-results js-search-results'>
         {
-        	list.map(function(enterprise, index) {
-        		return (
-                    <li key={index} className='search-result organization four columns'>
-                        <h2 key={index + 'title'} className="organization__title">{enterprise.title}</h2>
-                        <div key={index + 'description'} className="organization__description">{enterprise.description}</div>
-                        <div key={index + 'website'} className="organization__website"><a key={index + 'link'} href={enterprise.website}>Website</a></div>
-                    </li>
-        		);
-        	})
+          list.map(function(enterprise, index) {
+            return (
+                <li key={index} className='search-result organization four columns'>
+                <h2 key={index + 'title'} className="organization__title">{enterprise.title}</h2>
+                <div key={index + 'description'} className="organization__description">{enterprise.description}</div>
+                <div key={index + 'website'} className="organization__website"><a key={index + 'link'} href={enterprise.website}>Website</a></div>
+                </li>
+                );
+          })
         }
-      </ol>
-    );
+        </ol>
+        );
   }
 }
 
