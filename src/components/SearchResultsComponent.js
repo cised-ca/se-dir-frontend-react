ac215d21069ec00a77b955a90622573c3c51a0b0
Chimo <chimo@chromic.org>
2016-07-06 18:47:42 -0400

    Split SearchResults into Enterprise, EnterpriseRow
    Most grid CSS frameworks have the concept of a 'row'. So this way we can
    apply those classes on the EnterpriseRow container.

--- src/components/SearchResultsComponent.js
+++ src/components/SearchResultsComponent.js
@@ -1,36 +1,44 @@
 'use strict';
 
 import React from 'react';
+import EnterpriseRow from './EnterpriseRowComponent.js';
 
 require('lunr');
-require('styles//SearchResults.scss');
+require('styles/SearchResults.scss');
 
 class SearchResultsComponent extends React.Component {
   render() {
     var list = this.props.directory.enterprises,
       lunr_results,
       jsx = [],
-      enterprise;
+      enterprises = [];
 
     // Make sure the lunr index has been built
     if (this.props.lunr_index !== null) {
-      lunr_results = this.props.lunr_index.search(this.props.searchText),
-      lunr_results.forEach(function(result, index) {
-        enterprise = list[result.ref];
-        jsx.push(
-          <li key={index} className='search-result organization'>
-            <h2 key={index + 'title'} className="organization__title">{enterprise.title}</h2>
-            <div key={index + 'description'} className="organization__description">{enterprise.description}</div>
-            <div key={index + 'website'} className="organization__website"><a key={index + 'link'} href={enterprise.website}>Website</a></div>
-          </li>
-        );
+      lunr_results = this.props.lunr_index.search(this.props.searchText);
+
+      lunr_results.forEach(function(result) {
+        enterprises.push(list[result.ref]);
+      });
+
+      // FIXME: The number of cols is hard-coded here. And the relevant class
+      //        name is in EnterpriseRowComponent. It would be nice to make
+      //        this easier to configure.
+      enterprises.reduce(function(rows, enterprise, index) {
+        if (index % 4 === 0) {
+          rows.push([]);
+        }
+        rows[rows.length - 1].push(enterprise);
+        return rows;
+      }, []).map(function(row, index) {
+        jsx.push(<EnterpriseRow key={index} row={row} />);
       });
     }
 
     return (
-      <ol className='search-results js-search-results'>
+      <div className='search-results js-search-results'>
         {jsx}
-      </ol>
+      </div>
     );
   }
 }
