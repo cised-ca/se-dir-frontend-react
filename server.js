8131028cdd92198e388595886f1b6b8e0e0fb98f
Chimo <chimo@chromic.org>
2016-07-10 10:45:18 -0400

    build: Replace 'open' with 'opn'
    'open' is old, unmaintained and ships with a broken xdg-open script
    which breaks `npm start` on Linux.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('core-js/fn/object/assign');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
-const open = require('open');
+const open = require('opn');
 
 new WebpackDevServer(webpack(config), config.devServer)
 .listen(config.port, 'localhost', (err) => {
